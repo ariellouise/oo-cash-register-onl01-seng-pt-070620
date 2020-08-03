ef3f74a5ab55c042157564b85979fce3a9a94881
ariel murphy <arielladders@gmail.com>
2020-08-03 21:21:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,4 +15,13 @@ class CashRegister
     end 
   end 
   
+  def apply_discount 
+    if dicount > 0 
+    discount_on_total = (@total * @discount) / 100 
+      @total -= discount_on_total 
+      return "After the discount, the total comes to $#{@total}."
+    else 
+      return "There is no discount to apply." 
+    end 
+  end 
 end 
